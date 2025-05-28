2cfc5b974a3e2d3f733519b6de7ae2c34b320c9b
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-05-28 21:48:39 +0530

    added the question 3 for loop

---
+++ Question 3/index.cpp
@@ -0,0 +1,10 @@
+// how to print no from 1 too 100 using for loop 
+#include <iostream>
+using namespace std; // std::cout 
+int main () {
+    for (int i = 1; i<=100; i++) {
+        cout << i << " " ; 
+    }
+    return 0;
+}
+
