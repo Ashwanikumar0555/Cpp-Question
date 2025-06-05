4d120a2293370d54bb932e14136d530e65d400ba
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-06-05 14:16:29 +0530

    added th question 2

--- STL Question 1/index.cpp
+++ STL Question 1/index.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 int main() {
-    vector<int> numbers;
+ vector<int>numbers;
     int num;
+ cout <<"Enter 5 numbers :"<<endl;
+ // for loop 
+ for (int i=0; i<5; i++) {
+    cin >> num;
+    numbers.push_back(num);
+ }
 
-    cout << "Enter 5 numbers:" << endl;
-
-    for (int i = 0; i < 5; i++) {
-        cin >> num;
-        numbers.push_back(num);
-    }
-
-    cout << "The vector elements are: ";
-    for (int i = 0; i < numbers.size(); i++) {
-        cout << numbers[i] << " ";
-    }
-
-    return 0;
-} 
+ // Print the vector value
+ cout << "The vector Elements are: " << endl;
+ for (int i=0; i<numbers.size(); i++) {
+    cout << numbers[i] << " ";
+ }
+ return 0;
+ 
+}
