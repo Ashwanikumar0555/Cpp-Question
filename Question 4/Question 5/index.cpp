2d57d0c3b35839bef49695a0a2ef79e59937e973
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-06-10 22:18:49 +0530

    added the question 5

--- Question 4/Question 5/index.cpp
+++ Question 4/Question 5/index.cpp
@@ -21,3 +21,28 @@ int main() {
 
     return 0;
 }
+
+
+// Sort a vector
+// Implement a function to sort a std::vector<int> in ascending order using std::sort. give me simple solution 
+
+#include <iostream>
+#include <vector>
+#include <algorithm> // for std::sort
+
+void sortVector(std::vector<int>& vec) {
+    std::sort(vec.begin(), vec.end());
+}
+
+int main() {
+    std::vector<int> numbers = {5, 2, 9, 1, 5, 6};
+
+    sortVector(numbers);
+
+    std::cout << "Sorted vector: ";
+    for (int num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
