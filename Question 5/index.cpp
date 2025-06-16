d693602699fc2111930717cf8c831fc8f3069f81
Ashwani kumar <ashwanikumar05556@gmail.com>
2025-06-16 23:19:07 +0530

    added the question 5 reverse a vector

---
+++ Question 5/index.cpp
@@ -0,0 +1,23 @@
+// How to reverse a vector in c++ 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::vector<double> numbers = {1.1, 2.2, 3.3, 4.4, 5.5};
+
+    std::cout << "Original vector: ";
+    for (double num : numbers) {
+        std::cout << num << " ";
+    }
+
+    std::reverse(numbers.begin(), numbers.end());
+
+    std::cout << "\nReversed vector: ";
+    for (double num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
